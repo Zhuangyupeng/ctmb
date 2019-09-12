82c6df0733e54de45cbee7a05aa06541c829f1b5
zyp <zyp@ewhale.cn>
2019-09-12 16:00:56 +0800

    4

--- src/main/java/com/example/ctmb/model/Message.kt
+++ src/main/java/com/example/ctmb/model/Message.kt
@@ -1,12 +1,16 @@
 package com.example.ctmb.model
 
+import java.time.LocalDateTime
+
 class Message {
 
     companion object {
-        var TYPE_BAR = "通知栏推送"
-        var TYPE_APP = "APP内消息中心推送"
-        var SCOPE_ALL = "全部用户"
-        var SCOPE_SECTION = "指定用户"
+        val TYPE_BAR = "通知栏推送"
+        val TYPE_APP = "APP内消息中心推送"
+        val SCOPE_ALL = "全部用户"
+        var SCOPE_SECTION = "部分用户"
+        val STATUS_PENDING = "待发送"
+        val STATUS_SEND = "已发送"
     }
 
     var id: Long = 0
@@ -15,6 +19,7 @@ class Message {
     var subtitle: String? = null
     var scope: String? = null
     var type: String? = null
-
-
+    var createAt: LocalDateTime? = null
+    var image:String? = null
+    var user: User? = null
 }
