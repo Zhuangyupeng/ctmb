2c9e1ac50b61ee48421ed4c13c9cc6989a9a4c44
zyp <zyp@ewhale.cn>
2019-09-12 11:15:59 +0800

    4

---
+++ src/main/java/com/example/ctmb/model/Invitation.kt
@@ -0,0 +1,11 @@
+package com.example.ctmb.model
+
+class Invitation {
+    companion object {
+        const val STATUS_PENDING = "等待验证"
+        const val STATUS_ACCEPTED = "已添加"
+        const val STATUS_REJECTED = "拒绝"
+    }
+    
+    
+}
--- src/main/java/com/example/ctmb/model/User.kt
+++ src/main/java/com/example/ctmb/model/User.kt
@@ -11,9 +11,17 @@ import javax.persistence.Table
 @Table(name = "users")
 class User {
     companion object {
+        const val TYPE_MASTER = "主账号"
+        const val TYPE_SLAVE = "次账号"
+
+        const val ROLE_FATHER = "爸爸"
+        const val ROLE_MOTHER = "妈妈"
+        const val ROLE_GRANDFATHER = "爷爷"
+        const val ROLE_GRANDMOTHER = "奶奶"
 
         const val GENDER_MALE = "男"
         const val GENDER_FEMALE = "女"
+
         const val STATUS_ENABLE = "已启用"
         const val STATUS_DISABLE = "已禁用"
     }
@@ -22,7 +30,7 @@ class User {
     @Id
     @Column(name = "id")
     var id: Long = 0
-
+    var familyId:String? = null
     var phone: String? = null
     var password: String? =null
     var qqId: String? = null
@@ -39,6 +47,8 @@ class User {
     var district: String? = null
     var career: String? = null
     var comment: String? = null
+    var type: String? = null
+    var familyRole: String? = null
 
     var children: Children? = null
     var device:MutableSet<Device> = mutableSetOf()
